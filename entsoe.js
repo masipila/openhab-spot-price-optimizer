e8a1ebcffbc04b0124924e1bc7d60407703d0ade
Markus Sipilä <markus.sipila@iki.fi>
2024-02-03 18:42:08 +0200

    Change input arguments to be ZonedDateTime + support fetching prices both for a single or multiple days

--- entsoe.js
+++ entsoe.js
@@ -15,10 +15,10 @@ class Entsoe {
     /**
      * Reads the spot prices from Entso-E API.
      *
-     * @param string start
-     *   Start time for the Entso-E API call in format YYYYMMDDHHMM.
-     * @param string end
-     *   End time for the Entso-E API call in format YYYYMMDDHHMM.
+     * @param ZonedDateTime start
+     *   Start time for the Entso-E API call.
+     * @param ZonedDateTime end
+     *   End time for the Entso-E API call.
      * @param float tax
      *   VAT multiplier
      *
@@ -32,120 +32,152 @@ class Entsoe {
 	    return null;
 	}
 
-	const priceXml = this.makeApiCall(start, end);
-	const points = this.preparePoints(priceXml, tax);
+	const response = this.makeApiCall(start, end);
+	const points = this.parseResponse(response, tax);
 	return points;
     }
 
     /**
      * Makes an API call to Entso-E API to fetch the spot prices.
      *
-     * @param string start
+     * @param ZonedDateTime start
      *   Start time for the Entso-E API call.
-     * @param string end
+     * @param ZonedDateTime end
      *   End time for the Entso-E API call.
      *
      * @return string
      *   XML response from Ensto-E API.
      */
     makeApiCall(start, end) {
-	const http = Java.type("org.openhab.core.model.script.actions.HTTP");
-	console.log('entsoe.js: Making an API call to Entso-E API...');
+	// Spot prices are published from 00:00 to 00:00 on CET/CEST.
+	// Calculate the offset between local time zone and CET/CEST.
+	const cet = start.withZoneSameLocal(time.ZoneId.of("Europe/Berlin"));
+	const offset = time.ChronoUnit.HOURS.between(start, cet);
+	const startStr = start.plusHours(offset).format(time.DateTimeFormatter.ISO_INSTANT);
+	const endStr = end.plusHours(offset).format(time.DateTimeFormatter.ISO_INSTANT);
+	console.log("entsoe.js: Making an API call to Entso-E API for time interval " + startStr + "/" + endStr);
+
+	// Prepare the URL for the API call.
 	const url =
 	      'https://web-api.tp.entsoe.eu/api?' +
 	      'securityToken=' + this.config.entsoe.token +
 	      '&documentType=A44' +
 	      '&in_Domain=' + this.config.entsoe.zone +
 	      '&out_Domain=' + this.config.entsoe.zone +
-	      '&periodStart=' + start +
-	      '&periodEnd=' + end;
+	      '&TimeInterval=' + startStr + "/" + endStr;
 	console.debug("entsoe.js: URL for the API call: " + url);
 
-	let priceXml = '';
-	priceXml = http.sendHttpGetRequest(url, 10000);
+	const http = Java.type("org.openhab.core.model.script.actions.HTTP");
+	let response = http.sendHttpGetRequest(url, 10000);
 	console.debug("entsoe.js: Response XML from Entso-E API:");
-	console.debug(priceXml);
-	return priceXml;
+	console.debug(response);
+	return response;
     }
 
     /**
      * Parses the spot prices from the Entso-E XML response.
      *
-     * @param string priceXml
-     *   Entso-E response in XML format.
+     * @param string response
+     *   XML response from Entso-E API.
      * @param float tax
      *   Multiplier for VAT.
      *
      * @return array
-     *   Array of point objects. Empty array if spot prices can't be parsed.
+     *   Array of datetime-value pairs representing spot prices.
      */
-    preparePoints(priceXml, tax) {
+    parseResponse(response, tax) {
 	console.log('entose.js: transforming XML to JSON and parsing prices...');
 	const transformation = Java.type("org.openhab.core.transform.actions.Transformation");
-	let parsedPrices = [];
 	let prices = [];
+	let json = [];
 
 	// Transform the XML response to JSON for easier parsing.
 	try {
-	    prices = JSON.parse(transformation.transform('XSLT', 'xml2json.xsl', priceXml));
+	    json = JSON.parse(transformation.transform('XSLT', 'xml2json.xsl', response));
 	}
 	catch (exception) {
 	    console.error('entsoe.js: Error while transforming XML to JSON! Is xml2json.xsl present?');
+	    console.error(exception);
+	    return prices;
 	}
     
 	// API returns an Acknowledgement_MarketDocument when prices are not available.
-	if ('Acknowledgement_MarketDocument' in prices) {
+	if ('Acknowledgement_MarketDocument' in json) {
 	    try {
-		const error_text = prices['Acknowledgement_MarketDocument']['Reason']['text'];
-		console.error('entsoe.js: ' + error_text);
+		const error_text = json['Acknowledgement_MarketDocument']['Reason']['text'];
+		console.warn('entsoe.js: ' + error_text);
 	    }
 	    catch {
 		console.error('entsoe.js: Unexpected Acknowledgement_MarketDocument response!');
-		console.error(priceXml);
+		console.error(response);
+		return prices;
 	    }
 	}
 
 	// API returns Publication_MarketDocument when prices are available.
-	else if ('Publication_MarketDocument' in prices) {
+	else if ('Publication_MarketDocument' in json) {
 	    try {
-		// We assume here that multiple days have been requested.
-		let days = prices['Publication_MarketDocument']['TimeSeries'].length;
-		console.log("entsoe.js: Received time series for " + days + " days.");
-		for (let i = 0; i < days; i++) {
-		    let startUtc = prices['Publication_MarketDocument']['TimeSeries'][i]['Period']['timeInterval']['start'];
-		    console.debug("entsoe.js: start: " + startUtc);
-		    let points = prices['Publication_MarketDocument']['TimeSeries'][i]['Period']['Point'];
-		    let n = Object.keys(points).length
-
-		    // Loop through spot prices. Convert EUR / MWh to c / kWh and add tax.
-		    for (let j = 0; j < n; j++) {
-			let date = new Date(startUtc);
-			date.setUTCHours(date.getUTCHours() + j);
-			let price = points[j]['price.amount'] * tax / 10;
-			price = price.toFixed(4);
-			let point = {
-			    datetime: date.toISOString(),
-			    value: price
-			};
-			parsedPrices.push(point);
-			console.debug('entsoe.js: ' + date.toISOString() + ' ' + price + ' c/kWh');
+		// TimeSeries is either an array (multiple days) or an object (single day).
+		if (Array.isArray(json['Publication_MarketDocument']['TimeSeries'])) {
+		    let n = json['Publication_MarketDocument']['TimeSeries'].length;
+		    console.log("entsoe.js: Received time series for " + n + " days.");
+		    for (let i = 0; i < n; i++) {
+			let timeSeries = json['Publication_MarketDocument']['TimeSeries'][i];
+			prices = prices.concat(this.parseTimeSeries(timeSeries, tax));
 		    }
 		}
+		else {
+		    let timeSeries = json['Publication_MarketDocument']['TimeSeries'];
+		    prices = prices.concat(this.parseTimeSeries(timeSeries, tax));
+		}
 	    }
-	    catch {
+	    catch (exception){
 		console.error('entsoe.js: Unexpected Publication_MarketDocument response!');
-		console.error(priceXml);
+		console.error(response);
+		console.error(exception);
 	    }
 	}
 
 	// API did not return Acknowledgement_MarketDocument or Publication_MarketDocument
 	else {
 	    console.error('entsoe.js: Unexpected response type!');
-	    console.error(priceXml);
+	    console.error(response);
 	}
 
 	// Rerutrn the spot prices (or empty array)
-	return parsedPrices;
+	return prices;
+    }
+
+    /**
+     * Parses one TimeSeries element of the response.
+     *
+     * @param object timeSeries
+     *   Json object representing the time series of one day.
+     *
+     * @return array
+     *   Array of datetime-value pairs representing prices.
+     */
+    parseTimeSeries(timeSeries, tax) {
+	let prices = [];
+	let start = time.toZDT(timeSeries['Period']['timeInterval']['start']);
+	let resolution = time.Duration.parse(timeSeries['Period']['resolution']);
+	console.log("entsoe.js: Time series start: " + start + ", resolution: " + resolution);
+
+	// Loop through spot prices. Convert EUR / MWh to c / kWh and add tax.
+	let points = timeSeries['Period']['Point'];
+	for (let i = 0; i < points.length; i++) {
+	    let current = start.plus(resolution.multipliedBy(i));
+	    let datetime = current.format(time.DateTimeFormatter.ISO_INSTANT);
+	    let price = points[i]['price.amount'] * tax / 10;
+	    price = price.toFixed(4);
+	    let point = {
+		datetime: datetime,
+		value: price
+	    };
+	    prices.push(point);
+	    console.debug('entsoe.js: ' + datetime + ' ' + price + ' c/kWh');
+	}
+	return prices;
     }
 }
 
