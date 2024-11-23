c3d9f9fb0b42845afb79b12ca88f96c857947382
Markus Sipilä <markus.sipila@iki.fi>
2024-11-23 12:01:23 +0200

    Refactor control point cloner to use timeseries.

--- control-point-cloner.js
+++ control-point-cloner.js
@@ -1,5 +1,5 @@
 /**
- * Spot price optimizer, class for cloning control points for tomorrow if spot prices are not available.
+ * Spot price optimizer, class for cloning control points for tomorrow.
  */
 class ControlPointCloner {
 
@@ -10,25 +10,48 @@ class ControlPointCloner {
   }
 
   /**
-   * Adjusts the datetimes of the given control points 1 day forward.
+   * Checks if control points are missing for the provided control item.
    *
-   * @param array points
-   *   Array of control points as datetime-value pairs.
-   *
-   * @return array
-   *   Same control points with datetimes adjusted 1 day forward.
+   * @param Item controlItem
+   * @param ZonedDateTime tomorrowStart
    */
-  getClonedControlPoints(points) {
-    console.log('control-point-cloner.js: Cloning control points for tomorrow');
-    console.log('control-point-cloner.js: Source control points');
-    console.log(JSON.stringify(points));
-    for (let i=0; i < points.length; i++) {
-      let zdt = time.toZDT(points[i]['datetime']);
-      points[i]['datetime'] = zdt.plusDays(1).format(time.DateTimeFormatter.ISO_INSTANT);
+  missingTomorrow(controlItem, tomorrowStart) {
+    const tomorrowEnd = tomorrowStart.plusDays(1);
+    const expected = time.Duration.between(tomorrowStart, tomorrowEnd).toHours()*4;
+    const actual = controlItem.persistence.countBetween(tomorrowStart, tomorrowEnd);
+    if (actual < expected) {
+      console.log(`control-point-cloner.js: Tomorrow's control points missing for ${controlItem.name}!`);
+      return true;
     }
-    console.log('control-point-cloner.js: Control points after adjustment');
-    console.log(JSON.stringify(points));
-    return points;
+    return false;
+  }
+
+  /**
+   * Clones the control points 1 day forward.
+   *
+   * @param Item controlItem
+   * @param ZonedDateTime tomorrowStart
+   *
+   * @return TimeSeries
+   */
+  getClonedControlPoints(controlItem, todayStart) {
+    const tomorrowStart = todayStart.plusDays(1);
+
+    console.log('control-point-cloner.js: Cloning control points for one day onwards.');
+    console.log('control-point-cloner.js: Source control points:');
+    const source = controlItem.persistence.getAllStatesBetween(todayStart, tomorrowStart);
+    console.log(source);
+
+    // Clone the control points.
+    const ts = new items.TimeSeries('REPLACE');
+    for (let i = 0; i < source.length; i++) {
+      let zdt = source[i].timestamp.plusDays(1);
+      let value = String(source[i].numericState);
+      ts.add(zdt, value);
+    }
+    console.log('control-point-cloner.js: Cloned control points:');
+    console.log(ts);
+    return ts;
   }
 }
 
